119947d393e3b414b5f79d9c3d7603258e921531
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-03-11 11:35:48 -0300

    Simple structure for card user with animations added.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  animations: [
+    trigger('bodyExpansion', [
+      state('collapsed, void', style({ height: '0px', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition(
+        'expanded <=> collapsed, void => collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class UsersComponent implements OnInit {
+  state = 'collapsed';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggle(): void {
+    this.state = this.state === 'collapsed' ? 'expanded' : 'collapsed';
   }
-
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+// NAVIGATION
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { LayoutModule } from '@angular/cdk/layout';
+// CARD
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -14,6 +17,7 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatSidenavModule,
     MatListModule,
     LayoutModule,
+    MatCardModule,
   ],
   exports: [
     MatToolbarModule,
@@ -22,7 +26,7 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatSidenavModule,
     MatListModule,
     LayoutModule,
-
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
